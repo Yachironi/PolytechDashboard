2ad028e67ebf637f81b2bbdb3d2243faaf68374f
gblanc <guillaume.blanchard37@gmail.com>
2015-03-12 09:09:44 +0100

    database connection

--- src/utils/DatabaseConnection.php
+++ src/utils/DatabaseConnection.php
@@ -2,14 +2,23 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>title</title>
+    <title>Database Connection</title>
     <link rel="stylesheet" href ="style.css">
     <script src="script.js"></script>
   </head>
   <body>
-<?php
-echo "Test Cedric";
-?>
+	<?php
+	try{
+	    $pdo = new PDO('sqlite:'.dirname(__FILE__).'/database.sqlite');
+	    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // ERRMODE_WARNING | ERRMODE_EXCEPTION | ERRMODE_SILENT
+	    echo "Connection a la base de donnees SQLite reussie: ";
+	     
+	} catch(Exception $e) {
+	    echo "Impossible d'acceder à la base de donnees SQLite : ".$e->getMessage();
+	    die();
+	}
+	?>
   </body>
 </html>
 
