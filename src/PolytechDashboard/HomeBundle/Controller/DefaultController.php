8c67051f7b277e96184128ce9d670befaa40a1f8
CedricPortaneri <cportaneri@gmail.com>
2015-04-17 09:12:58 +0200

    MAJ gitIgnore

--- src/PolytechDashboard/HomeBundle/Controller/DefaultController.php
+++ src/PolytechDashboard/HomeBundle/Controller/DefaultController.php
@@ -6,7 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller {
 	public function indexAction() {
-		return $this->render ( 'PolytechDashboardHomeBundle:Default:index.html.twig', array('prenom' => 'Prenom','nom' => 'Nom','id' => '1'));
+		$session = $this->getRequest ()->getSession ();
+		// stocke un attribut pour une future requête
+		$session->set ( 'idEtudiant', '1' );
+		
+		/* appel du generateur de note du controlleur */
+		$noteController = $this->get ( 'noteController' );
+		//print_r ( "BEFORE" );
+		$myGrades = $noteController->indexAction ( 1, $this );
+		//print_r ( "AFTER" );
+		
+		return $this->render ( 'PolytechDashboardHomeBundle:Default:index.html.twig', array (
+				'prenom' => 'Guillaume',
+				'nom' => 'Blanchard',
+				'id' => '1',
+				'myGrades' => $myGrades 
+		) );
+		
 		// return $this->render('PolytechDashboardHomeBundle:Default:index.html.twig', array('name' => $name));
 	}
 }
