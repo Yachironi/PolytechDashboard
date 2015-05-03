af8e9f25c6115263942390541dd8a31c5e1e6846
gblanc <guillaume.blanchard37@gmail.com>
2015-05-03 12:55:36 +0200

    mapping controller -> twig pour récupérer les input

--- src/PolytechDashboard/HomeBundle/Controller/TacheController.php
+++ src/PolytechDashboard/HomeBundle/Controller/TacheController.php
@@ -69,49 +69,132 @@ class TacheController extends Controller {
 	
 	public function submitFormAction(Request $request) {
 		if ($request->getMethod () == 'POST') {
-			$typeForm = substr($request->get ( 'list_form' ),-1);
+			$typeForm = substr ( $request->get ( 'list_form' ), - 1 );
 			
-			$destinataire = $request->get ( 'destinataire_form'.$typeForm );
-			$objet = $request->get ( 'objet_form'.$typeForm );
-			/* TODO Importance*/				
-			switch ($typeForm){
-				case 2 : case 3 :
+			$destinataire = $request->get ( 'destinataire_form' . $typeForm );
+			$objet = $request->get ( 'objet_form' . $typeForm );
+			
+			/* TODO Importance */
+			// $importance =
+			
+			switch ($typeForm) {
+				case 2 :
 					/* durée */
+					$duree = $request->get ( 'duree_absence' );
+					
+					/* motif */
+					$duree = $request->get ( 'motif_justification_absence' );
+					
+					/* fichier */
+					$file = $request->get ( 'absence_InputFile' );
 					
 					/* échéance */
+					if ($request->get ( 'checkbox_form2' ) == true) {
+						$echeance = $request->get ( 'echeance_form2' );
+					}
+					
+					break;
+				case 3 :
+					/* date et heure du RDV */
+					$duree = $request->get ( 'date_rdv' );
+					
+					/* motif */
+					$duree = $request->get ( 'motif_rdv' );
+					
+					/* échéance */
+					if ($request->get ( 'checkbox_form3' ) == true) {
+						$echeance = $request->get ( 'echeance_form3' );
+					}
+					
 					break;
 				case 4:
 					/* type du devoir rendu */
+					$type = $request->get ( 'list_devoirs' );
 					
 					/* nom du devoir */
-				
+					$nom = $request->get ( 'objet_form4' );
+					
+					/* fichier */
+					$file = $request->get ( 'renduDevoir_InputFile' );
+					
 					/* commentaire */
+					$commentaire = $request->get ( 'commentaire_rendu_devoir' );
+					
+					break;				
 				case 5:
 					/* type d'inscritpion*/
 					$typeForm = $request->get ( 'list_inscription' );
-					/* switch/case TOEIC/Bourse/M2R/Sport/Polytech */
 					
 					/* motif */
+					$duree = $request->get ( 'motif_inscription' );
 					
-					/* pièce jointe */
+					/* fichier */
+					$file = $request->get ( 'inscription_InputFile' );
+					
+					/* échéance */
+					if ($request->get ( 'checkbox_form5' ) == true) {
+						$echeance = $request->get ( 'echeance_form5' );
+					}
+					
+					break;			
 				case 6:
-					/* durée du stage */
+					/* dates de stage */
+					$dates_stage = $request->get ( 'dates_stage' );
 					
-					/* échéance optionelle */
+					/* détail */
+					$detail = $request->get ( 'detail_stage' );
+					
+					/* fichier */
+					$file = $request->get ( 'validStage_InputFile' );
+					
+					/* échéance */
+					if ($request->get ( 'checkbox_form6' ) == true) {
+						$echeance = $request->get ( 'echeance_form6' );
+					}
+					
+					break;				
 				case 7:	
 					/* remarque*/
+					$remarque_stage = $request->get ( 'remarque_stage' );
 					
 					/* fichier */
+					$file = $request->get ( 'pdf_convention_InputFile' );
 					
 					/* échéance */
+					if ($request->get ( 'checkbox_form7' ) == true) {
+						$echeance = $request->get ( 'echeance_form7' );
+					}
 					
-				case 8:
+					break;
+				case 8 :
 					/* text */
+					$text = $request->get ( 'texte_form8' );
 					
 					/* fichier */
+					$file = $request->get ( 'taskPerso_InputFile' );
 					
 					/* échéance */
+					if ($request->get ( 'checkbox_for8' ) == true) {
+						$echeance = $request->get ( 'echeance_form8' );
+					}
+					
+					break;				
+				case 9 :
+					/* text */
+					$text = $request->get ( 'text_autre_demande' );
+					
+					/* fichier */
+					$file = $request->get ( 'demandeNonRepertoriee_InputFile' );
+
+					/* échéance */
+					if ($request->get ( 'checkbox_form6' ) == true) {
+						$echeance = $request->get ( 'echeance_form6' );
+					}
+
+					break;				
 			}
+			
+			
 		}
 	}
 }
